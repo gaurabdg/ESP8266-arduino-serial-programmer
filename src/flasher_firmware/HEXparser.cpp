6266dc94dfb72f24f4245d83e1389458d77b8a2c
Gaurab Dasgupta <gaurabdg0@gmail.com>
2017-06-30 20:51:22 +0530

    Implemented HEX parsing

--- src/flasher_firmware/HEXparser.cpp
+++ src/flasher_firmware/HEXparser.cpp
@@ -1,33 +1,13 @@
 #include "HEXparser.h"
 #include "Arduino.h"
 
-void HEXparser::ParseRecord()
+//initiate address
+HEXparser::HEXparser()
 {
-	recType = RecordType(record);
-
-	if(recType == 0)
-	{
-		extractData(record, length);
-	}
-
-	if(recType == 1)
-	{
-		FileEnd();
-	}
+	address[0] = 0x00;
+	address[1] = 0x00;
 }
 
-int HEXparser::RecordAddress(byte* record)
-{
-	char holder[5];
-
-	holder[0] = record[3];
-	holder[1] = record[4];
-	holder[2] = record[5];
-	holder[3] = record[6];
-	holder[4] = '\0';
-
-	return strtol(holder, 0, 16);
-}
 
 int HEXparser::RecordLength(byte* record)
 {
@@ -40,6 +20,23 @@ int HEXparser::RecordLength(byte* record)
 	return strtol(holder, 0, 16);
 }
 
+void HEXparser::RecordAddress(byte* record)
+{
+	char holder[3];
+	
+	holder[2] = '\0';
+
+	holder[0] = record[3];
+	holder[1] = record[4];
+	address[0] = strtol(buff, 0, 16);
+
+	holder[0] = record[5];
+	holder[1] = record[6];
+	address[1] = strtol(buff, 0, 16); 
+}
+
+
+
 int HEXparser::RecordType(byte* record)
 {
 	char holder[3];
@@ -49,4 +46,74 @@ int HEXparser::RecordType(byte* record)
 	holder[2] = '\0';
 
 	return strtol(holder, 0, 16);	
+}
+
+byte* HEXparser::extractData(byte* record, int len)
+{
+	int begin = 9; 
+	int end = (len*2) + begin; //data is in sets of 2
+	char holder[3];
+	holder[2] = '\0';
+
+	for(int i=begin;begin<end;i=i+2)
+	{
+		holder[0] = record[i];
+		holder[1] = record[i+1];
+
+		RawPage[index] = strtol(holder, 0, 16);
+		index++;
+	}
+}
+
+void HEXparser::FileEnd()
+{
+	address[1] += 0x40;
+	if(address[1] == 0)
+	{
+		address[0] += 0x1;
+	}
+
+	while(index<128)
+	{
+		RawPage[index] = 0xFF;
+		index++ ;
+	}
+}
+
+void HEXparser::ParseRecord(byte* record)
+{
+	recType = RecordType(record);
+
+	if(recType == 0)
+	{
+		length = RecordLength(record);
+		extractData(record, length);
+
+		if(index == 128)
+		{
+			address[1] += 0x40;
+
+			if(address[1] == 0)
+			{
+					address[0] += 0x1;
+			}
+
+			index = 0;
+		}
+	}
+
+	if(recType == 1)
+	{
+		FileEnd();
+	}
+}
+
+byte* HEXparser::FetchRaw()
+{
+	return RawPage;
+}
+
+byte* HEXparser::FetchAddress()
+{
+	return address;
 }
--- src/flasher_firmware/HEXparser.h
+++ src/flasher_firmware/HEXparser.h
@@ -5,17 +5,23 @@
 class HEXparse
 {
 	public:
-		HEXparse();
-		ParseRecord(byte* data); // Import data from spiffs.open() in web server.
-		
+		HEXparser();
+		void ParseRecord(byte* data); // Import data from spiffs.open() in web server.
+		byte* FetchRaw();
+		byte* FetchAddress();
+
 	private:
 		int recType = 0; // Here only 0/1 is needed-data present/end of file.
 		int length = 0;
+		int index = 0;
+		byte RawPage[128];
+		byte address[2];
 
 		/* Intel HEX file format info */
-		int RecordType(byte* record);
-		int RecordAddress(byte* record);
 		int RecordLength(byte* record);
+		void RecordAddress(byte* record);
+		int RecordType(byte* record);
+
 		//                             //
 		byte* extractData(byte* record, int len);
 		void FileEnd();
