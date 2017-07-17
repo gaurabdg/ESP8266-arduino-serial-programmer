c0615141757f95f30761dd6756084f29dcc916e3
Gaurab Dasgupta <gaurabdg0@gmail.com>
2017-07-17 23:47:13 +0530

    renamed, meh

--- src/webpage/js/custom-file-input.js
+++ src/webpage/js/custom-file-input.js
@@ -33,4 +33,38 @@
 	});
 }( document, window, 0 ));
 
+/*  Function for highlighting row of staged file */
 
+function highlight_row() 
+{
+    var table = document.getElementById('table');
+    var cells = table.getElementsByTagName('td');
+
+    for (var i = 0; i < cells.length; i++) 
+    {
+        // Take each cell
+        var cell = cells[i];
+        // do something on onclick event for cell
+        cell.onclick = function () 
+        {
+            // Get the row id where the cell exists
+            var rowId = this.parentNode.rowIndex;
+
+            var rowsNotSelected = table.getElementsByTagName('tr');
+            for (var row = 0; row < rowsNotSelected.length; row++) 
+            {
+                rowsNotSelected[row].style.backgroundColor = "";
+                rowsNotSelected[row].classList.remove('selected');
+            }
+            var rowSelected = table.getElementsByTagName('tr')[rowId];
+            rowSelected.style.backgroundColor = "#e5a734";
+            rowSelected.className += " selected";
+
+
+            
+        }
+    }
+
+} //end of function
+
+window.onload = highlight_row;
